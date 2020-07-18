157fbe568e43eee5b75cc314bd2d69aa6dc6796f
Franco Benitez <franco_casla_benitez@hotmail.com>
2020-07-18 20:00:00 -0300

    agregando rango de valores

--- src/componentes/Filtro/Filtro.js
+++ src/componentes/Filtro/Filtro.js
@@ -9,6 +9,8 @@ import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
 import './Filtro.css';
 
 const useStyles = makeStyles((theme) => ({
@@ -25,6 +27,7 @@ const Filtro = ({setPropiedades}) => {
     const [operaciones, setOperaciones] = useState([]);
     const [categorias, setCategorias] = useState([]);
     const [localidades, setLocalidades] = useState([]);
+    const [check, setCheck] = useState(false);
     useEffect(() => {
         const getData = ()=>{
             fetch('http://104.197.241.81:3000/operaciones').then(res=>res.json()).then(operaciones=>{
@@ -49,6 +52,11 @@ const Filtro = ({setPropiedades}) => {
         valor:0
     });
 
+    const [rango, setRango] = useState({
+        minPrecio:0,
+        maxPrecio:0
+    });
+
     const [error, setError] = useState(false);//completar campos
     const [sinPropiedad, setSinPropiedad] = useState(false);
     const [loader, setLoader] = useState(false);
@@ -70,7 +78,11 @@ const Filtro = ({setPropiedades}) => {
         setSinPropiedad(false);
         setLoader(true);
         document.getElementsByTagName('body')[0].style.overflowY = 'hidden';
-        fetch(`http://104.197.241.81:3000/filtrar_todo/${busqueda.idLocalidad}/${busqueda.idCategoria}/${busqueda.idOperacion}/${busqueda.precio}/${busqueda.moneda}`).then(res=>res.json()).then(response=>{
+        let query = `http://104.197.241.81:3000/filtrar_todo/${busqueda.idLocalidad}/${busqueda.idCategoria}/${busqueda.idOperacion}/${busqueda.precio}/${busqueda.moneda}`;
+        if(check){
+            query += `?minPrecio=${rango.minPrecio}&maxPrecio=${rango.maxPrecio}`;
+        }
+        fetch(query).then(res=>res.json()).then(response=>{
             setLoader(false);
             document.getElementsByTagName('body')[0].style.overflowY='visible';
             if(!response.data){
@@ -78,6 +90,20 @@ const Filtro = ({setPropiedades}) => {
                 return;
             };
             setPropiedades(response.data);
+        })
+    }
+
+    const handleChangeCheck = event=>{
+        if(check){
+            return setCheck(false);
+        }
+        return setCheck(true);
+    };
+
+    const handleChangeCheckValues = event=>{
+        setRango({
+            ...rango,
+            [event.target.name]:event.target.value
         })
     }
 
@@ -167,19 +193,36 @@ const Filtro = ({setPropiedades}) => {
                             </Select>
                         </FormControl>
                     </div>
-                    <div className="col-12 col-sm-4 col-xl-2">
-                        <FormControl className={classes.formControl + ' w-100'}>
-                            <TextField id="valor" 
-                                        label="Valor"
-                                        type="number" 
-                                        name="valor"
-                                        value={busqueda.valor}
-                                        onChange={handleChange} />
-                        </FormControl>
-                        {/* <Button variant="contained" color="secondary" type="submit" className="botonSubmit w-100 mt-3">
-                            Buscar
-                        </Button> */}
+                    <div className="col-12 col-sm-4 col-xl-2 pt-3">
+                        <FormControlLabel
+                            control={<Checkbox checked={check} onChange={handleChangeCheck} name="habilitarRango" />}
+                            label="Habilitar rango de precio"
+                        />
                     </div>
+                    {(!check)?null:
+                    <>
+                        <div className="col-12 col-sm-2">
+                            <FormControl className={classes.formControl + ' w-100'}>
+                                <TextField id="valor" 
+                                            label="Min. valor"
+                                            type="number" 
+                                            name="minPrecio"
+                                            value={rango.minPrecio}
+                                            onChange={handleChangeCheckValues} />
+                            </FormControl>
+                        </div>
+                        <div className="col-12 col-sm-2">
+                            <FormControl className={classes.formControl + ' w-100'}>
+                                <TextField id="valor" 
+                                            label="Max. valor"
+                                            type="number" 
+                                            name="maxPrecio"
+                                            value={rango.maxPrecio}
+                                            onChange={handleChangeCheckValues} />
+                            </FormControl>
+                        </div>
+                    </>
+                    }
                     <div className="col-12 text-center mt-3">
                         <Button variant="contained" color="secondary" type="submit" className="botonSubmit mt-3">
                                 Buscar

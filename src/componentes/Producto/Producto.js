dc82b48a027cb3e3ff8b9eb04d61e84eebd5d112
Franco Benitez <fhbenitez@hcdn.gob.ar>
2021-07-14 12:13:43 -0300

    'update'

--- src/componentes/Producto/Producto.js
+++ src/componentes/Producto/Producto.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
@@ -10,7 +10,7 @@ import CardMedia from '@material-ui/core/CardMedia';
 import { red } from '@material-ui/core/colors';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { Link } from 'react-router-dom';
 
 import './Producto.css';
@@ -18,14 +18,21 @@ import './Producto.css';
 const useStyles = makeStyles({
     media: {
       height: 140,
+      width: '100%',
+      objectFit:'cover'
     },
     avatar: {
         backgroundColor: red[500],
+    },
+    progress: {
+       textAlign:'center'
     }
   });
 
 const Producto = ({propiedad}) => {
     const classes = useStyles();
+    const [load, setLoad] = useState(false);
+
     let imagen = `${propiedad.header}`;
     let descripcion = propiedad.descripcion;
     if (descripcion.length>300) {
@@ -34,6 +41,10 @@ const Producto = ({propiedad}) => {
     
     const verPropiedad = id=>{
         window.location.assign(`/propiedad/${id}`);
+    }
+
+    const handleLoad = e => {
+        setLoad(true);
     }
     
     return (
@@ -49,18 +60,20 @@ const Producto = ({propiedad}) => {
             />
             {(propiedad.mostrarEstado==='no')?null:<span className="labelEstado">{propiedad.estado}</span>}
             <CardActionArea>
-                <CardMedia
-                className={classes.media}
-                image={imagen}
-                title="Contemplative Reptile"
+                <img
+                    className={classes.media + `${load ? '' : 'd-none'}`}
+                    src={imagen}
+                    alt={descripcion}
+                    onLoad={handleLoad}
                 />
-                <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {descripcion}
-                </Typography>
+                { !load ? <div className={classes.progress}><CircularProgress color="secondary" /></div> : null }
+                <CardContent>   
+                    <Typography gutterBottom variant="h5" component="h2">
+                        
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {descripcion}
+                    </Typography>
                 </CardContent>
             </CardActionArea>
             <CardActions className="card-actions">

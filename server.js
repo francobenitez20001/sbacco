9b53c6d5025c6a4959eb3ae497560fc3ae70b95f
Franco Benitez <fhbenitez@hcdn.gob.ar>
2021-07-14 16:01:37 -0300

    update server

--- server.js
+++ server.js
@@ -5,30 +5,8 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname,'build')));
 
-app.get("/",(req,res)=>{
-    console.log(req.url);
-    res.sendFile(path.join(__dirname,"build","index.html"));
-});
-app.get("/nosotros",(req,res)=>{
-    console.log(req.url);
-    res.sendFile(path.join(__dirname,"build","index.html"));
-});
-app.get("/contacto",(req,res)=>{
-    console.log(req.url);
-    res.sendFile(path.join(__dirname,"build","index.html"));
-});
-app.get("/propiedad/:id",(req,res)=>{
-    res.sendFile(path.join(__dirname,"build","index.html"));
-})
-app.get("/propiedades",(req,res)=>{
-    console.log(req.url);
-    res.sendFile(path.join(__dirname,"build","index.html"));
-});
-app.get("/propiedad/:id",(req,res)=>{
-    res.sendFile(path.join(__dirname,"build","index.html"));
-});
-app.use(function(req, res, next) {
-    res.sendFile(path.join(__dirname,"build","index.html"));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.listen(4200);

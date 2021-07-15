b7cb555214df40dbe84ca58cbefb3cbb74b0c45c
franco <francobenitez980@gmail.com>
2021-07-15 03:06:23 +0000

    update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname,'build')));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(__dirname, 'build', "index.html"));
 });
 
 app.listen(4200);
